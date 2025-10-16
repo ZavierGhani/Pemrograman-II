d42d4fd2d6fdf87a08a613e08dbb0f67bd2eced6
ZavierGhani <zavierghani02@gmail.com>
2025-10-16 17:55:05 +0800

    Add files via upload

---
+++ Modul 3/PRAK301_2410817210014_ZavierPutraNataGhani/Dadu.java
@@ -0,0 +1,20 @@
+package praktikum3.PRAK301_2410817210014_ZavierPutraNataGhani;
+
+class Dadu {
+    private int nilai;
+    private final int min = 1;
+    private final int max = 6;
+
+    Dadu() {
+        acakNilai();
+    }
+
+    private void acakNilai() {
+        int random_int = (int)Math.floor(Math.random() * (max - min + 1) + min);
+        this.nilai = random_int;
+    }
+
+    public int getNilai() {
+        return nilai;
+    }
+}
---
+++ Modul 3/PRAK302_2410817210014_ZavierPutraNataGhani/Negara.java
@@ -0,0 +1,45 @@
+package praktikum3.PRAK302_2410817210014_ZavierPutraNataGhani;
+
+
+import java.util.HashMap;
+
+class Negara {
+    private String nama;
+    private String jenis;
+    private String namaPemimpin;
+    private int tanggal;
+    private int bulan;
+    private int tahun;
+
+    // presiden / perdana menteri
+    public Negara(String nama, String jenis, String namaPemimpin, int tanggal, int bulan, int tahun) {
+        this.nama = nama;
+        this.jenis = jenis;
+        this.namaPemimpin = namaPemimpin;
+        this.tanggal = tanggal;
+        this.bulan = bulan;
+        this.tahun = tahun;
+    }
+
+    // untuk monarki
+    public Negara(String nama, String jenis, String namaPemimpin) {
+        this.nama = nama;
+        this.jenis = jenis;
+        this.namaPemimpin = namaPemimpin;
+    }
+
+    public int getBulan() {
+        return bulan;
+    }
+
+    public void tampilkan(HashMap<Integer, String> daftarBulan) {
+        if (jenis.equalsIgnoreCase("monarki")) {
+            System.out.println("Negara " + nama + " mempunyai Raja bernama " + namaPemimpin + "\n");
+        } else {
+            String bulanTeks = daftarBulan.get(bulan);
+            String jabatan = jenis.equalsIgnoreCase("presiden") ? "Presiden" : "Perdana Menteri";
+            System.out.println("Negara " + nama + " mempunyai " + jabatan + " bernama " + namaPemimpin  +  
+                    "\nDeklarasi Kemerdekaan pada Tanggal " + tanggal + " " + bulanTeks + " " + tahun + "\n");
+        }
+    }
+}
