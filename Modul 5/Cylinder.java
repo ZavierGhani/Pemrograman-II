a20897ce5e2f704ce6426398fdb904ab72a5c2d8
ZavierGhani <zavierghani02@gmail.com>
2025-12-24 21:24:26 +0800

    Add files via upload

---
+++ Modul 5/Cylinder.java
@@ -0,0 +1,26 @@
+package praktikum5;
+
+public class Cylinder extends Shape
+{
+    private double radius;
+    private double height;
+
+    public Cylinder(double r, double h)
+    {
+        super("Cylinder");
+        radius = r;
+        height = h;
+    }
+
+    public double area()
+    {
+        return Math.PI * radius * radius * height;
+    }
+
+    public String toString()
+    {
+        return super.toString() + 
+               " with radius " + radius + 
+               " and height " + height;
+    }
+}
---
+++ Modul 5/Shape.java
@@ -0,0 +1,18 @@
+package praktikum5;
+
+public abstract class Shape 
+{
+    protected String shapeName;
+
+    public Shape(String name)
+    {
+        shapeName = name;
+    }
+
+    public abstract double area();
+
+    public String toString()
+    {
+        return shapeName;
+    }
+}
